57c30779ac0da9a9eee7abd7e94291ba6be8570e
nbullonav <nbullon@live.com>
2025-03-25 23:16:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,27 +3,34 @@ from datetime import datetime
 from meteostat import Stations, Daily, Monthly
 import matplotlib.pyplot as plt
 
+#Retrieving Weather stations located in Peru
 stations = Stations()
 stations = stations.region('PE').fetch() # weather stations in Peru
 
+# station IDs capture into a dataframe
 pd = stations
 pd['ident'] = pd.index
 conds = ~ (pd.monthly_start.isna() & pd.monthly_end.isna())
 filteredpd = pd[conds]
 
+# sending station IDs to a list
 stationlist = filteredpd['ident'].to_list()
 
+# Defining a start and end date for our dataset
 start = datetime(1950,1,1)
 end =   datetime(2025,2,28)
 data = Monthly(stationlist ,start, end )
+# Command to fetch Monthly Station data
 data = data.fetch()
 
-pd = data
-finaldata = pd.join(filteredpd, on ='station',how='left')
+# retrieved data into a dataframe
+dfdata = data 
+# We want to make a left join of the Station list w/ the data 
+finaldata = dfdata.join(filteredpd, on ='station',how='left')
 
 # Define needed fields
 whichfields = ['tavg','prcp','wspd','pres','tsun','name','latitude','longitude']
-# Define csv filename
+# Define .csv filename
 csvfile = 'stations data.csv'
 
 finaldata.to_csv(csvfile, columns=whichfields)
